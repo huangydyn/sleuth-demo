d6bdf600d17d5e28aaa1798aef1608a3784956a8
huangyan <yxhuang@thoughtworks.com>
2020-01-20 11:18:49 +0800

    add hystrix

--- spring-server-consumer-one/src/main/java/com/tracing/representation/HelloController.java
+++ spring-server-consumer-one/src/main/java/com/tracing/representation/HelloController.java
@@ -4,11 +4,8 @@ import com.tracing.representation.response.SpringConsumerTwo;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Map;
 
 @Slf4j
 @RestController
@@ -23,8 +20,7 @@ public class HelloController {
 
     @ApiOperation("hello")
     @GetMapping("/hello")
-    public String hello(@RequestHeader Map<String, String> headers) {
-        headers.forEach((k, v) -> log.info("Header: {}, Value: {}", k, v));
+    public String hello() {
         log.info("Spring EnterPoint request received");
         springConsumerTwo.hello();
         return "Spring Consumer One say hello";
--- spring-server-enterpoint/src/main/java/com/tracing/EnterPointApplication.java
+++ spring-server-enterpoint/src/main/java/com/tracing/EnterPointApplication.java
@@ -2,10 +2,12 @@ package com.tracing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableAsync
+@EnableHystrix
 @SpringCloudApplication
 @SuppressWarnings("PMD.UseUtilityClass")
 @EnableFeignClients(basePackages = "com.tracing")
